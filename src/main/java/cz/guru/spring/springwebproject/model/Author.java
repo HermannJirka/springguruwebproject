bda36d5b34b493b6f8a28a03bf3541b333d5f251
hermannjirka <hermann.jirka@gmail.com>
2019-11-12 23:56:31 +0100

    spring guru project

---
+++ src/main/java/cz/guru/spring/springwebproject/model/Author.java
@@ -0,0 +1,56 @@
+package cz.guru.spring.springwebproject.model;
+
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+   @ManyToMany(mappedBy = "authors")
+    private Set<Book> book = new HashSet<>();
+
+    public Author() {
+    }
+
+    public Author(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Author(String firstName, String lastName, Set<Book> book) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.book = book;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Set<Book> getBook() {
+        return book;
+    }
+
+    public void setBook(Set<Book> book) {
+        this.book = book;
+    }
+
+}
